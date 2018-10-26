e1c13d8adee4bd028514b7875877ce488bcb0dd8
IamSanjid <iamsanjidhaque@gmail.com>
2018-10-26 13:17:19 +0600

    Added starter choosing option and Fixed Mart guy issue

--- config.lua
+++ config.lua
@@ -2,3 +2,4 @@ KANTO_STARTER_ID  = math.random(1,3) -- 1: Fatvegan, 2: Salamender, 3: Aquaturtl
 KANTO_FOSSIL_ID =math.random(1,2) -- 1: Helix, 2: Dome
 DOJO_POKEMON_ID =math.random(1,2) -- 1: Hitmonchan, 2: Hitmonlee
 BIKE_COLOR_ID =math.random(1,4) -- 1:red 2:blue 3:green 4:yellow
+starterName = "Squirtle"
